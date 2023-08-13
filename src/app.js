befce4bac0d66c0fe7d055a208994d41eda7b0c4
ElizaValle <beteduvalle@gmail.com>
2023-08-13 12:38:30 -0300

    requisito POST /tweets

--- src/app.js
+++ src/app.js
@@ -16,6 +16,18 @@ app.post("/sign-up", (req, res) => {
     res.send("OK")
 })
 
+app.post("/tweets", (req, res) => {
+    const { username, tweet } = req.body
+
+    // verifica se usuário está cadastrado comparando username do tweet com username da conta
+    const usuarioCadastrado = users.find((usuario) => usuario.username === username)
+    if (!usuarioCadastrado) return res.send("UNAUTHORIZED")
+
+    // salva tweet no array tweets
+    tweets.push({ username, tweet })
+    res.send("OK")
+})
+
 
 const PORT = 5000
 app.listen(PORT, () => console.log(`Servidor rodando na porta ${PORT}`))
